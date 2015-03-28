5e715ce2e518f85f327705277cad10bf1f84db27
Spin Rodriguez <spin@askfavor.com>
2015-03-28 01:06:53 -0500

    trade model

---
+++ api/controllers/TradeController.js
@@ -0,0 +1,11 @@
+/**
+ * TradeController
+ *
+ * @description :: Server-side logic for managing trades
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
