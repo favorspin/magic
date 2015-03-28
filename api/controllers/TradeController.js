c77f10a2011587fc5c8e2531da47d970895ec9f1
Spin Rodriguez <spin@askfavor.com>
2015-03-28 13:47:18 -0500

    trade update

--- api/controllers/TradeController.js
+++ api/controllers/TradeController.js
@@ -6,6 +6,33 @@
  */
 
 module.exports = {
-	
+
+    myTrades: function(req,res) {
+
+        if(req.param('user')) {
+            var userId = req.param('user');
+        } else {
+            var userId = null;
+        }
+
+        Trade.find().where({or:[{receiver:userId},{sender:userId}]})
+            .populate('receiverCards')
+            .populate('senderCards')
+            .populate('sender')
+            .populate('receiver')
+            .exec(function(err,trades) {
+                if(err) {
+                    res.badRequest('There was an error. Please try again.', '500');
+                } else {
+                    if(trades.length === 0) {
+                        res.notFound({trades: null}, 'trade/myTrades');
+                    } else {
+                        res.ok({trades: trades}, 'trade/myTrades');
+                    }
+                }
+            });
+
+    }
+
 };
 
